c370e994a174f7d0905081023ef2f270434db473
Yasin <arafath7yasin@gmal.com>
2022-01-19 21:28:11 -0800

    v2.1

--- pages/index.js
+++ pages/index.js
@@ -50,6 +50,26 @@ export default function Home() {
         <title>Note App</title>
       </Head>
 
+   
+
+      <div id="outer-container">
+        <div className="note-app min-h-[96vh]">
+          <main id="page-wrap">
+            <Note />
+          </main>
+        </div>
+        <footer>
+          <p className="text-center text-white mt-8">
+            Developed by
+            <Link href="https://www.linkedin.com/in/yasin-arafath-80ab22207/">
+              <a className="text-orange-400 hover:text-orange-600">
+                {" "}
+                Yasin Arafath
+              </a>
+            </Link>
+          </p>
+        </footer>
+      </div>
       {popout ? (
         <></>
       ) : (
@@ -71,25 +91,6 @@ export default function Home() {
           </button>
         </div>
       )}
-
-      <div id="outer-container">
-        <div className="note-app min-h-[96vh]">
-          <main id="page-wrap">
-            <Note />
-          </main>
-        </div>
-        <footer>
-          <p className="text-center text-white mt-8">
-            Developed by
-            <Link href="https://www.linkedin.com/in/yasin-arafath-80ab22207/">
-              <a className="text-orange-400 hover:text-orange-600">
-                {" "}
-                Yasin Arafath
-              </a>
-            </Link>
-          </p>
-        </footer>
-      </div>
     </>
   );
 }
