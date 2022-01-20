5d4f4e5462ad9034178fb25f041ba6688877674d
Yasin <arafath7yasin@gmal.com>
2022-01-19 22:22:06 -0800

    bismilah

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,52 @@
+import React from "react";
+
 import Head from "next/head";
 
 import Note from "../components/Note";
 
 import Link from "next/link";
+import { FaTimes } from "react-icons/fa";
 
 export default function Home() {
+  let addBtn = React.useRef();
+
+  let [popout, setpopout] = React.useState(false);
+
+  let handlePop = () => {
+    setpopout(true);
+  };
+
+  React.useEffect(() => {
+    window.onload = () => {
+      let deferredPrompt;
+
+      window.addEventListener("beforeinstallprompt", (e) => {
+        e.preventDefault();
+        deferredPrompt = e;
+        setpopout(false);
+      });
+
+      addBtn.current?.addEventListener("click", () => {
+        setpopout(true);
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choice) => {
+          if (choice.outcome === "accepted") {
+            console.log("User accepted");
+          } else {
+            console.log("User dismissed");
+          }
+        });
+        deferredPrompt = null;
+      });
+
+      window.addEventListener("appinstalled", () => {
+        setpopout(true);
+        deferredPrompt = null;
+        console.log("PWA was installed");
+      });
+    };
+  }, []);
+
   return (
     <>
       <Head>
@@ -29,6 +71,27 @@ export default function Home() {
           </p>
         </footer>
       </div>
+      {popout ? (
+        <></>
+      ) : (
+        <div className="a2hsPop popinstall will-change-transform">
+          <button
+            onClick={handlePop}
+            className="text-white active:text-red-700 p-2 absolute top-0 right-0"
+          >
+            <FaTimes />
+          </button>
+          <h2 className="text-white text-2xl">
+            For better experience install in mobile
+          </h2>
+          <button
+            ref={addBtn}
+            className="text-white bg-blue-800 active:bg-blue-900 px-4 py-1 text-xl rounded mt-2"
+          >
+            Install
+          </button>
+        </div>
+      )}
     </>
   );
 }
